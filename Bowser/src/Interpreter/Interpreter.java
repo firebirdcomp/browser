c4991ef2070503b7d11b8f2bfffd75cab4a58095
Felipe <felipe.bock@hotmail.com>
2019-05-21 21:32:00 -0300

    Regex is working

--- Bowser/src/Interpreter/Interpreter.java
+++ Bowser/src/Interpreter/Interpreter.java
@@ -70,8 +70,14 @@ public class Interpreter {
 //            verify if tag has brothers
             if ("".equals(over)) {
 //                verify if there is tags in content
-                if (contentInsideTag.matches("\\<(\\w+)\\>")) {
+                Pattern pattern2= Pattern.compile("\\<(\\w+)\\>");
+                Matcher matcher2 = pattern.matcher(contentInsideTag);
+                if (matcher2.matches()) {
                     return generateTree(contentInsideTag, node);
+                }
+                else
+                {
+                    //TODO
                 }
             } else {
 //                remove the closing tag
@@ -85,79 +91,7 @@ public class Interpreter {
     
     public String clearHTML(String html)
     {
-        html = "<html>\n" +
-"<head>\n" +
-"    <title>Exemplo de página</title>\n" +
-"</head>\n" +
-"<body>\n" +
-"    <h1><u>Modelo de documento HTML</u></h1>\n" +
-"    \n" +
-"    <!-- Cabeçalhos -->\n" +
-"    <h1>Hello <s>World</s> Gurizada! </h1>\n" +
-"    <h2>Hello <s>World</s> Abiguinhos! </h2>\n" +
-"    <h3>Hello <s>World</s> Cambada! </h3>\n" +
-"    <!-- Linha de separação -->\n" +
-"    \n" +
-"    <!-- \n" +
-"        A quebra de linha feita no HTML não irá influenciar o texto.\n" +
-"        Apenas irá quebrar a linha caso seja utilizado.\n" +
-"    -->\n" +
-"    <p>\n" +
-"        Lorem, ipsum <b>dolor sit amet</b> consectetur adipisicing elit.\n" +
-"        Deserunt<i> tempore ea, architecto suscipit voluptas sed</i> beatae  \n" +
-"        dicta incidunt minima sint unde hic, iure porro animi ab nihil \n" +
-"        error. Enim, repellat.\n" +
-"        </p>\n" +
-"    <!-- Tabelas -->\n" +
-"    <h2>Tabela</h2>\n" +
-"    <table>\n" +
-"        <!-- Cabeçalho da tabela -->\n" +
-"        <thead>\n" +
-"            <!-- Cria uma linha -->\n" +
-"            <tr>\n" +
-"                <!-- <th> destaca o texto e marca como um elemento do cabeçalho da tabela -->\n" +
-"                    <th>Nome</th>\n" +
-"                    <th>Idade</th>\n" +
-"                    <th>País</th>\n" +
-"                </tr>\n" +
-"        </thead>\n" +
-"        <!-- Corpo da tabela -->\n" +
-"        <tbody>\n" +
-"            <tr>\n" +
-"                <td>Pedro</td>\n" +
-"                <td>12</td>\n" +
-"                <td>Brasil</td>\n" +
-"            </tr>\n" +
-"            <tr>\n" +
-"                <td>Arduino</td>\n" +
-"                <td>9</td>\n" +
-"                <td>Brasil</td>\n" +
-"            </tr>\n" +
-"            <tr>\n" +
-"                <td>Mateus</td>\n" +
-"                <td>18</td>\n" +
-"                <td>Brasil</td>\n" +
-"            </tr>\n" +
-"        </tbody>\n" +
-"    </table>\n" +
-"    <!-- Listas -->\n" +
-"    <h2>Listas</h2>\n" +
-"    <h3>Lista comum</h3>\n" +
-"    <ul>\n" +
-"        <li>Item 1</li>\n" +
-"        <li>Item 2</li>\n" +
-"        <li>Item 3</li>\n" +
-"        <li>Item 4</li>\n" +
-"    </ul>\n" +
-"    <h3>Lista com ordenação</h3>\n" +
-"    <ol>\n" +
-"        <li>Item 1</li>\n" +
-"        <li>Item 2</li>\n" +
-"        <li>Item 3</li>\n" +
-"        <li>Item 4</li>\n" +
-"    </ol>\n" +
-"</body>\n" +
-"</html> ";
+        html = "<html><head><title></title></head><body><div><h1>TITULO</h1><p>TEXTO DENTRO<b>DO</b>PARAGRAFO</p></div></body></html>";
      
 //        remove new lines from HTML
         String _html = html.replaceAll("\\n", "");
@@ -165,6 +99,8 @@ public class Interpreter {
 //        remove comments from HTML
          _html = _html.replaceAll("<!--.*?-->", "");
             System.out.println("Removed comentaries: " + _html);
+            
+         _html = _html.replaceAll(" ", "");
         return _html;
     }    
 }
