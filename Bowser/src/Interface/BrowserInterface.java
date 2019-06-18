e846e980b4d92bc600f072edd5e44800cc64eac2
felpygames <felipe.bock@hotmail.com>
2019-06-18 18:40:56 -0300

    Small changes

--- Bowser/src/Interface/BrowserInterface.java
+++ Bowser/src/Interface/BrowserInterface.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Browser;
+package Interface;
 
 import Interpreter.Interpreter;
 import Render.Render;
